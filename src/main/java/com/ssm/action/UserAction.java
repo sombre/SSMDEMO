7a45511a285da0cbaec57d3bb9d571899be1d2f9
John <hinsdiff@hotmail.com>
2017-12-28 12:45:35 +0800

    包含初步mybatis,spring,springmvc整合的开发环境

--- src/main/java/com/ssm/action/UserAction.java
+++ src/main/java/com/ssm/action/UserAction.java
@@ -1,13 +1,14 @@
 package com.ssm.action;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssm.model.User;
 import com.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,22 +16,43 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/user")
 public class UserAction {
-
-    @Autowired
     private UserService userService;
-
+    @Autowired
+    public UserAction(UserService userService) {
+        this.userService = userService;
+    }
     @RequestMapping("/showUser")
-    public String selectUser(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ModelAndView selectUser(HttpServletRequest request, HttpServletResponse response, @RequestParam int uid) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-//        System.out.println(Long.parseLong(request.getParameter("id")));
-//        long userId = Long.parseLong(request.getParameter("id"));
-//        long userId = Long.parseLong(request.getParameter("id"));
-        User user = this.userService.selectUserById(1);
+        User user = this.userService.selectUserById(uid);
+        modelAndView.addObject("user",user);
+        modelAndView.setViewName("index");
+//        this.userService.addPlayer("male","John Smith","1000");
 //        ObjectMapper mapper = new ObjectMapper();
 //        response.getWriter().write(mapper.writeValueAsString(user));
 //        response.getWriter().close();
-        request.setAttribute("user",user);
-        return "index";
+//        request.setAttribute("user",user);
+        return modelAndView;
     }
+
+
+    public ModelAndView registerUser(int uid,String userName,String passWord,String email,String gender) throws Exception{
+        User tmp = new User();
+        ModelAndView mv = new ModelAndView();
+        tmp.setUid(uid);
+        tmp.setUsername(userName);
+        tmp.setEmail(email);
+
+        tmp.setPassword(passWord);
+        tmp.setGender(gender);
+
+        return mv;
+
+    }
+
+
+
+
 }
